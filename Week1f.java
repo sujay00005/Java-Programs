75d2a7e54a20e0743849e5f75a5fe9abfecfa0d9
Sujay Biswas <64656592+sujay00005@users.noreply.github.com>
2021-10-02 00:30:14 +0530

    Add files via upload
    Sum of all submatrices of order 2x2

---
+++ Week1f.java
@@ -0,0 +1,38 @@
+// Given a NxN 2-D matrix, the task to find the sum of all the submatrices.
+
+import java.util.Scanner;
+
+public class Week1f {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter the number of rows.");
+        int row = input.nextInt();
+
+        System.out.println("Enter the number of columns.");
+        int column = input.nextInt();
+
+        int arr[][] = new int[row][column];
+        System.out.println("Enter the elements of the array");
+
+        for(int i=0; i< row; i++){
+            for(int j = 0; j<column; j++){
+                arr[i][j] = input.nextInt();
+            }
+        }
+
+        System.out.println(matrixSum( arr, row, column));
+    }
+
+    public static int matrixSum(int arr[][], int row, int column){
+        int ans = 0;
+
+        for(int i=0; i<row; i++){
+            for(int j=0; j<column; j++){
+                ans = ans + arr[i][j]*(i+1)*(j+1)*(row-i)*(column-j); 
+            }
+        }
+        return ans;
+    }
+
+}
