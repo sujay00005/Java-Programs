6a5e24d4515a6360411e2b235bb57bca0652ee6e
Sujay Biswas <64656592+sujay00005@users.noreply.github.com>
2021-12-08 12:14:26 +0530

    Add files via upload
    Done in O(n)

--- PairDivisibleBy4.java
+++ PairDivisibleBy4.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class PairDivisibleBy4 {
@@ -15,13 +16,47 @@ public class PairDivisibleBy4 {
 
         input.close();
 
-        int count=0;
-        for (int i = 0; i <n; i++) {
-            for (int j = i+1; j <n; j++) {
-                if((a[i]+a[j])%4==0) count++;
-            }
+        int k=4;
+        int[] count = new int[k]; //since divisible by 4
+
+        Arrays.fill(count, 0);
+
+        for(int i=0; i<n; i++){
+            count[a[i]%k]++;
         }
 
-        System.out.println(count);
+        int ans =0;
+        ans = ans + (count[0]*( count[0] -1))/2;
+        
+        for (int i = 1; i <= (k-1)/2; i++) {
+            ans = ans + count[i]*count[k-i];
+        }
+
+        if(k%2==0){
+            ans = ans+ (count[k/2] *(count[k/2]-1))/2;
+        }
+        // else{
+        //     ans = ans +count[k/2]*count[k/2 +1];
+        // }
+
+        System.out.println(ans);
     }
 }
+
+
+
+
+
+
+
+
+//O(N^2)
+
+// int count=0;
+//         for (int i = 0; i <n; i++) {
+//             for (int j = i+1; j <n; j++) {
+//                 if((a[i]+a[j])%4==0) count++;
+//             }
+//         }
+
+//         System.out.println(count);
