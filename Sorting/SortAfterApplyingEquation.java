e7bd84a94d8dcf30e2183005d6a93d1a75462458
Sujay <sujay00005@gmail.com>
2022-05-11 22:53:34 +0530

    deleting comment for a file

--- Sorting/SortAfterApplyingEquation.java
+++ Sorting/SortAfterApplyingEquation.java
@@ -29,15 +29,16 @@ public class SortAfterApplyingEquation {
     }
 
     public static void sortArray(int array[], int a, int b, int c) {
-        int monotonicPoint = -b / (2 * a);
+        // int monotonicPoint = -b / (2 * a);
         for (int i = 0; i < array.length; i++) {
             array[i] = (a * array[i] * array[i]) + (b * array[i]) + c;
         }
-        merge(array, monotonicPoint);
+        merge(array);
+        // monotonicPoint);
 
     }
 
-    public static void merge(int a[], int point) {
+    public static void merge(int a[]) {
         int i = 0, j = a.length - 1, c = 0;
         int temp[] = new int[a.length];
 
@@ -52,30 +53,4 @@ public class SortAfterApplyingEquation {
             a[k] = temp[k];
         }
     }
-    // public static void merge(int a[], int point) {
-    // int temp[] = new int[a.length];
-    // if (point > a[a.length - 1] || point < a[0])
-    // return;
-
-    // int i = 0, j = a.length - 1, c = 0;
-    // while (a[i] < point && a[j] >= point) {
-    // if (a[i] > a[j])
-    // temp[c++] = a[j--];
-    // else
-    // temp[c++] = a[i++];
-    // }
-
-    // while (a[i] < point)
-    // temp[c++] = a[i++];
-
-    // while (a[j] >= point) {
-    // temp[c++] = a[j--];
-    // }
-
-    // for (int k = 0; k < temp.length; k++) {
-    // a[k] = temp[k];
-    // System.out.println(temp[k]);
-    // }
-
-    // }
 }
